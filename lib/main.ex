0133dbee3dfcd66c914fffe56d234f23f31cb0bb
Shah Nawaz <shuvo1824@hotmail.com>
2025-01-06 23:39:25 +0600

    stage 1 complete

--- lib/main.ex
+++ lib/main.ex
@@ -10,11 +10,11 @@ defmodule Server do
     IO.puts("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    #
-    # # Since the tester restarts your program quite often, setting SO_REUSEADDR
-    # # ensures that we don't run into 'Address already in use' errors
-    # {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
-    # {:ok, _client} = :gen_tcp.accept(socket)
+
+    # Since the tester restarts your program quite often, setting SO_REUSEADDR
+    # ensures that we don't run into 'Address already in use' errors
+    {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
+    {:ok, _client} = :gen_tcp.accept(socket)
   end
 end
 
